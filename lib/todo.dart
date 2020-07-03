d68410350262b4f3e0e2df92d4db191de91fc04c
Sumej <sumej@hotmail.com>
2020-07-03 21:41:33 +0530

    adding files

---
+++ lib/todo.dart
@@ -0,0 +1,5 @@
+class Todo {
+  String title;
+  bool isDone = false;
+  Todo({this.title, this.isDone = false});
+}
---
+++ lib/todo_list.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/todo.dart';
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  List<Todo> todos = [];
+  TextEditingController controller = new TextEditingController();
+
+  _toggleTodo(Todo todo, bool isChecked) {
+    setState(() {
+      todo.isDone = isChecked;
+    });
+  }
+
+  Widget _buildItem(BuildContext context, int index) {
+    final todo = todos[index];
+
+    return CheckboxListTile(
+      value: todo.isDone,
+      title: Text(todo.title),
+      onChanged: (bool isChecked) {
+        _toggleTodo(todo, isChecked);
+      },
+    );
+  }
+
+  _addTodo() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('New Item'),
+            content: TextField(
+              controller: controller,
+              autofocus: true,
+            ),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Cancel'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  setState(() {
+                    final todo = new Todo(title: controller.value.text);
+                    todos.add(todo);
+                    controller.clear();
+                    Navigator.of(context).pop();
+                  });
+                },
+                child: Text('Add'),
+              ),
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Todo List')),
+      body: ListView.builder(
+        itemBuilder: _buildItem,
+        itemCount: todos.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: _addTodo,
+      ),
+    );
+  }
+}
